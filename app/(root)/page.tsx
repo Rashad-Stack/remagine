2ea6647a63c6d0ef9a39384a675469a9ce72657f
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-02-29 05:40:16 +0600

    Update layout and add UserButton to home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
