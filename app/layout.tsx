f59ce594a963ae8542275162789c54475de883b0
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-02-29 07:50:47 +0600

    Update title and add MongoDB and Mongoose dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const IBMPlex = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Remagine",
+  title: "Reimagine",
   description: "Effortlessly Enhance Your Photos with AI",
 };
 
