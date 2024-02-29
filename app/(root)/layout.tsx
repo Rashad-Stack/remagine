0d5593502684cdcdf04d61349a49a458f497b788
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-02-29 06:20:18 +0600

    Add Sidebar component and update layout structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import Sidebar from "@/components/shared/Sidebar";
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="root">
-      <div className="root-container">
-        <div className="root-wrapper">{children}</div>
-      </div>
+      <Sidebar />
+      {/* <MobileNav/> */}
+
+      <section className="root-container">
+        <div className="wrapper">{children}</div>
+      </section>
     </main>
   );
 }
